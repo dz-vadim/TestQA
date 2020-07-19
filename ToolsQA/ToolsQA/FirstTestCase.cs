c926bf1eabd8c14823bda12c44a80ba2f2250f43
dz-vadim <46889822+dz-vadim@users.noreply.github.com>
2020-07-20 01:00:11 +0300

    Add files via upload

---
+++ ToolsQA/ToolsQA/FirstTestCase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ToolsQA
+{
+    class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+            NUnitTest test = new NUnitTest();
+            test.Initialize();
+            test.TestApp();
+
+
+
+            //IWebDriver driver = new ChromeDriver();
+            //driver.Navigate().GoToUrl("https://www.google.com");
+            //driver.Manage().Window.Maximize();
+            //driver.Close();
+            //driver.Quit();
+
+        }
+    }
+}
---
+++ ToolsQA/ToolsQA/NUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace ToolsQA
+{
+    class NUnitTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestApp()
+        {
+            driver.Url = "https://google.com.ua";
+            driver.FindElement(By.Name("q")).Click();
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("q")).SendKeys("selenium");
+            driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+    }
+}
